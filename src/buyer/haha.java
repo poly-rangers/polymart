bd2756f23e805878136be8080332654548b164ed
gjvlio <gjdr.2005@gmail.com>
2025-01-11 01:43:53 +0800

    refersh bin

---
+++ src/buyer/haha.java
@@ -0,0 +1,35 @@
+package buyer;
+import java.awt.Color;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class haha extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JTextField txtDitoSearchBar;
+
+	public haha(JPanel contentPane) {
+
+		setBackground(Color.WHITE);
+        setBounds(100, 100, 414, 660);
+        SpringLayout panelLayout = new SpringLayout();
+        setLayout(panelLayout);
+        
+        // Icon + title
+        ImageIcon originalImage = new ImageIcon(this.getClass().getResource("/polypup_buyer.icon.png"));
+        Image scaledImage = originalImage.getImage().getScaledInstance(150, 47, Image.SCALE_SMOOTH);
+        JLabel startupImage = new JLabel(new ImageIcon(scaledImage));
+        panelLayout.putConstraint(SpringLayout.NORTH, startupImage, 24, SpringLayout.NORTH, this);
+        panelLayout.putConstraint(SpringLayout.WEST, startupImage, 16, SpringLayout.WEST, this);
+        add(startupImage);
+
+}
