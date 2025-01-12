3b216dbb18d81e8a34e2a59d9017d65979a439b8
gjvlio <gjdr.2005@gmail.com>
2025-01-12 10:18:52 +0800

    naming conventions... nanaman

--- src/TermsConditionsPanel.java
+++ src/TermsConditionsPanel.java
@@ -31,8 +31,8 @@ public class TermsConditionsPanel extends JPanel implements ActionListener{
 		
 		setBackground(Color.WHITE);
         setBounds(100, 100, 414, 660);
-        SpringLayout panelLayout = new SpringLayout();
-        setLayout(panelLayout);
+        SpringLayout pnlTermsConditions = new SpringLayout();
+        setLayout(pnlTermsConditions);
         
         // Choose image based on userRole
         String strImagePath = userRole.equalsIgnoreCase("Buyer")
@@ -43,15 +43,15 @@ public class TermsConditionsPanel extends JPanel implements ActionListener{
         ImageIcon imgIconOriginalImage = new ImageIcon(this.getClass().getResource(strImagePath));
         Image imgScaled = imgIconOriginalImage.getImage().getScaledInstance(150, 47, Image.SCALE_SMOOTH);
         JLabel lblStartupImage = new JLabel(new ImageIcon(imgScaled));
-        panelLayout.putConstraint(SpringLayout.NORTH, lblStartupImage, 24, SpringLayout.NORTH, this);
-        panelLayout.putConstraint(SpringLayout.WEST, lblStartupImage, 16, SpringLayout.WEST, this);
+        pnlTermsConditions.putConstraint(SpringLayout.NORTH, lblStartupImage, 24, SpringLayout.NORTH, this);
+        pnlTermsConditions.putConstraint(SpringLayout.WEST, lblStartupImage, 16, SpringLayout.WEST, this);
         add(lblStartupImage);
 
         // Terms & Conditions label
         JLabel lblTermsHeader = new JLabel("Terms & Conditions");
-        panelLayout.putConstraint(SpringLayout.WEST, lblTermsHeader, 0, SpringLayout.WEST, this);
-        panelLayout.putConstraint(SpringLayout.EAST, lblTermsHeader, 414, SpringLayout.WEST, this);
-        panelLayout.putConstraint(SpringLayout.NORTH, lblTermsHeader, 119, SpringLayout.NORTH, this);
+        pnlTermsConditions.putConstraint(SpringLayout.WEST, lblTermsHeader, 0, SpringLayout.WEST, this);
+        pnlTermsConditions.putConstraint(SpringLayout.EAST, lblTermsHeader, 414, SpringLayout.WEST, this);
+        pnlTermsConditions.putConstraint(SpringLayout.NORTH, lblTermsHeader, 119, SpringLayout.NORTH, this);
         lblTermsHeader.setForeground(Color.BLACK);
         lblTermsHeader.setFont(new Font("Montserrat Medium", Font.PLAIN, 24));
         lblTermsHeader.setHorizontalAlignment(SwingConstants.CENTER);
@@ -85,10 +85,10 @@ public class TermsConditionsPanel extends JPanel implements ActionListener{
         scrollPane.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
 
         // Set constraints for the scroll pane
-        panelLayout.putConstraint(SpringLayout.NORTH, scrollPane, 16, SpringLayout.SOUTH, lblTermsHeader);
-        panelLayout.putConstraint(SpringLayout.WEST, scrollPane, 19, SpringLayout.WEST, this);
-        panelLayout.putConstraint(SpringLayout.SOUTH, scrollPane, -100, SpringLayout.SOUTH, this); // Adjusted for button
-        panelLayout.putConstraint(SpringLayout.EAST, scrollPane, -22, SpringLayout.EAST, this);
+        pnlTermsConditions.putConstraint(SpringLayout.NORTH, scrollPane, 16, SpringLayout.SOUTH, lblTermsHeader);
+        pnlTermsConditions.putConstraint(SpringLayout.WEST, scrollPane, 19, SpringLayout.WEST, this);
+        pnlTermsConditions.putConstraint(SpringLayout.SOUTH, scrollPane, -100, SpringLayout.SOUTH, this); // Adjusted for button
+        pnlTermsConditions.putConstraint(SpringLayout.EAST, scrollPane, -22, SpringLayout.EAST, this);
         add(scrollPane);
 
         // Create the "I Agree" button
@@ -98,8 +98,8 @@ public class TermsConditionsPanel extends JPanel implements ActionListener{
         btnAgreeButton.setForeground(Color.WHITE);
         btnAgreeButton.setFocusPainted(false);
         btnAgreeButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-        panelLayout.putConstraint(SpringLayout.NORTH, btnAgreeButton, 10, SpringLayout.SOUTH, scrollPane);
-        panelLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, btnAgreeButton, 0, SpringLayout.HORIZONTAL_CENTER, this);
+        pnlTermsConditions.putConstraint(SpringLayout.NORTH, btnAgreeButton, 10, SpringLayout.SOUTH, scrollPane);
+        pnlTermsConditions.putConstraint(SpringLayout.HORIZONTAL_CENTER, btnAgreeButton, 0, SpringLayout.HORIZONTAL_CENTER, this);
         btnAgreeButton.addActionListener(this);
         add(btnAgreeButton);
 	}
