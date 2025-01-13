1e7460ec83536b4634bdaa4ba5e02daabe8d7785
Julianna Leila Castillo <119785135+nintendocicc@users.noreply.github.com>
2025-01-14 00:42:51 +0800

    Sign up buttons radius modified.

--- src/buyer/BuyerSignupPanel.java
+++ src/buyer/BuyerSignupPanel.java
@@ -202,7 +202,7 @@ public class BuyerSignupPanel extends JPanel implements ActionListener {
     	add(btnPrivacyPolicy);
 
         // Sign up button
-        btnSignUp = new RoundButton("Sign Up", 30);
+        btnSignUp = new RoundButton("Sign Up", 45);
         panelLayout.putConstraint(SpringLayout.NORTH, btnSignUp, 22, SpringLayout.SOUTH, btnTermsConditions);
         panelLayout.putConstraint(SpringLayout.WEST, btnSignUp, 10, SpringLayout.WEST, labelUploadCOR);
         panelLayout.putConstraint(SpringLayout.EAST, btnSignUp, 0, SpringLayout.EAST, labelUploadCOR);
